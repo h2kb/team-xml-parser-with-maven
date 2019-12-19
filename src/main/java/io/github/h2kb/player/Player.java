7cdc9fa48831c2e95c68ee33475fbf9a9d89f1db
Miсhail h2kb Bereza <berezama@gmail.com>
2019-12-20 03:17:16 +0800

    Initial commit

---
+++ src/main/java/io/github/h2kb/player/Player.java
@@ -0,0 +1,48 @@
+package io.github.h2kb.player;
+
+public class Player {
+    private String name;
+    private String surname;
+    private int age;
+    private String team;
+    private String role;
+
+    public Player(String name, String surname, int age, String team, String role) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.team = team;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", team='" + team + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
